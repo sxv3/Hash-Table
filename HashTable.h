c997d988740d79cccddc8b6cff1c1862a1fa921c
sxv3 <66145858+sxv3@users.noreply.github.com>
2025-02-06 01:55:50 -0800

    Add files via upload

--- HashTable.h
+++ HashTable.h
@@ -3,22 +3,29 @@
 
 #include "Student.h"
 
+//Hash table header
+//created by Tai Wong, 2/6/25
+
+//node struct for linked list
 struct Node {
     Student* student;
     Node* next;
 };
 
+//hash table class
 class HashTable {
 private:
+  //array of linked list
     Node* *table;
     int size;
+  //functions
     int hashFunction(int id);
     void rehash();
 
 public:
     HashTable();
     ~HashTable();
-    
+  //prototypes
     void addStudent(Student* student);
     void printStudents();
     void deleteStudent(int id);
